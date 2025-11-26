16ba8b2725700f2e022b9b010f895a4f61f0f9d0
XuanPhuc269 <tranxuanphuc269@gmail.com>
2025-11-26 09:19:16 +0700

    Refactor educator routes and add purchase schema; implement educator dashboard data retrieval

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ const app = express();
 
 // Connect to Database
 await connectDB();
-// await connectCloudinary();
+await connectCloudinary();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-// app.use(clerkMiddleware());
+app.use(clerkMiddleware());
 
 // Routes
 app.get("/", (req, res) => res.send("API is running..."));
 app.post("/clerk", express.json(), clerkWebhooks);
-// app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/educator', express.json(), educatorRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
