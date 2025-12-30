254ad1e0af07717794d5d40fa945d15047765f9b
phunh1901 <phunh1901@gmail.com>
2025-12-30 23:53:30 +0700

    add quiz

--- models/Course.js
+++ models/Course.js
@@ -1,43 +1,59 @@
 import mongoose from "mongoose";
 
+const quizQuestionSchema = new mongoose.Schema(
+  {
+    questionText: { type: String, required: true },
+    options: [{ type: String, required: true }],
+    correctAnswerIndex: { type: Number, required: true }, // Index of the correct option in the options array
+  },
+  { _id: false }
+);
+
 const lectureSchema = new mongoose.Schema(
-    {
-        lectureId: { type: String, required: true },
-        lectureTitle: { type: String, required: true },
-        lectureDuration: { type: Number, required: true }, // duration in minutes
-        lectureUrl: { type: String, required: true },
-        isPreviewFree: { type: Boolean, default: false },
-    }, { _id: false }
+  {
+    lectureId: { type: String, required: true },
+    lectureTitle: { type: String, required: true },
+    lectureDuration: { type: Number, required: true }, // duration in minutes
+    lectureUrl: { type: String, required: true },
+    isPreviewFree: { type: Boolean, default: false },
+    lectureOrder: { type: Number, required: true },
+    quiz: {
+      isEnabled: { type: Boolean, default: false },
+      questions: [quizQuestionSchema],
+    },
+  },
+  { _id: false }
 );
 
 const chapterSchema = new mongoose.Schema(
-    {
-        chapterId: { type: String, required: true },
-        chapterOrder: { type: Number, required: true },
-        chapterTitle: { type: String, required: true },
-        chapterContent: [lectureSchema],
-    }, { _id: false }
+  {
+    chapterId: { type: String, required: true },
+    chapterOrder: { type: Number, required: true },
+    chapterTitle: { type: String, required: true },
+    chapterContent: [lectureSchema],
+  },
+  { _id: false }
 );
 
 const courseSchema = new mongoose.Schema(
-    {
-        courseTitle: { type: String, required: true },
-        courseDescription: { type: String, required: true },
-        courseThumbnail: { type: String },
-        coursePrice: { type: Number, required: true },
-        isPublished: { type: Boolean, default: true },
-        discount: { type: Number, required: true, min: 0, max: 100 },
-        courseContent: [chapterSchema],
-        courseRatings: [
-            {
-                userId: { type: String }, rating: { type: Number, min: 1, max: 5 }
-            }
-        ],
-        educator: { type: String, ref: "User", required: true },
-        enrolledStudents: [
-            { type: String, ref: "User" }
-        ],
-    }, { timestamps: true, minimize: false }
+  {
+    courseTitle: { type: String, required: true },
+    courseDescription: { type: String, required: true },
+    courseThumbnail: { type: String },
+    coursePrice: { type: Number, required: true },
+    isPublished: { type: Boolean, default: true },
+    discount: { type: Number, required: true, min: 0, max: 100 },
+    courseContent: [chapterSchema],
+    courseRatings: [
+      {
+        userId: { type: String },
+        rating: { type: Number, min: 1, max: 5 },
+      },
+    ],
+    educator: { type: String, ref: "User", required: true },
+    enrolledStudents: [{ type: String, ref: "User" }],
+  },
+  { timestamps: true, minimize: false }
 );
 
 const Course = mongoose.model("Course", courseSchema);
